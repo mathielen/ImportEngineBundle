0b290e0c2760210b12df4ec79067a0357abe5108
Markus Thielen <info@logicx.de>
2014-12-29 14:33:34 +0100

    composer update AbstractFileStorageProvider can now handle simple strings as selections createdby can be used to build importruns through importbuilder

--- Command/ImportCommand.php
+++ Command/ImportCommand.php
@@ -1,15 +1,20 @@
 <?php
 namespace Mathielen\ImportEngineBundle\Command;
 
+use Mathielen\DataImport\Event\ImportItemEvent;
 use Mathielen\ImportEngine\Exception\InvalidConfigurationException;
 use Mathielen\ImportEngine\Import\ImportBuilder;
 use Mathielen\ImportEngine\Import\Run\ImportRunner;
 use Mathielen\ImportEngine\Storage\StorageLocator;
 use Mathielen\ImportEngine\ValueObject\ImportConfiguration;
+use Mathielen\ImportEngine\ValueObject\ImportRun;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Helper\ProgressBar;
+use Symfony\Component\Console\Helper\Table;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class ImportCommand extends ContainerAwareCommand
@@ -21,7 +26,9 @@ class ImportCommand extends ContainerAwareCommand
             ->setDescription('Imports data with a definied importer')
             ->addArgument('importer', InputArgument::REQUIRED, 'id/name of importer')
             ->addArgument('source_id', InputArgument::REQUIRED, "id of source. Different StorageProviders need different id data.\n- upload, directory: \"<path/to/file>\"\n- doctrine: \"<id of query>\"\n- service: \"<service>.<method>[?arguments_like_url_query]\"")
-            ->addArgument('source_provider', InputArgument::OPTIONAL, 'id of source provider', 'default');
+            ->addArgument('source_provider', InputArgument::OPTIONAL, 'id of source provider', 'default')
+            ->addOption('context', 'c', InputOption::VALUE_OPTIONAL, 'Supply optional context information to import. Supply key-value data in query style: key=value&otherkey=othervalue&...')
+        ;
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
@@ -30,6 +37,7 @@ class ImportCommand extends ContainerAwareCommand
             throw new InvalidConfigurationException("No importengine builder service has been found. Did you register the bundle in AppKernel and configured at least one importer in config?");
         }
 
+        $progress = new ProgressBar($output);
         $importerName = $input->getArgument('importer');
         $sourceProvider = $input->getArgument('source_provider');
         $sourceId = $this->parseSourceId($input->getArgument('source_id'));
@@ -39,19 +47,65 @@ class ImportCommand extends ContainerAwareCommand
         $storageSelection = $storageLocator->selectStorage($sourceProvider, $sourceId);
         $importConfiguration = new ImportConfiguration($storageSelection, $importerName);
 
+        $output->writeln("Commencing import using importer <info>$importerName</info> with source provider <info>$sourceProvider</info> and source id <info>$sourceId</info>");
+
         /** @var ImportBuilder $importBuilder */
         $importBuilder = $this->getContainer()->get('mathielen_importengine.import.builder');
-        $importBuilder->build($importConfiguration);
+        $importRun = $importBuilder->build($importConfiguration, 'CLI');
+
+        //copy info from storage to import run
+        if (!empty($input->getOption('context'))) {
+            $context = [];
+            parse_str($input->getOption('context'), $context);
+            $importRun->setContext($context);
+        }
+        $importRun->setInfo((array) $storageLocator->getStorage($storageSelection)->info());
+
+        //status callback
+        $this->getContainer()->get('event_dispatcher')->addListener('data-import.read', function (ImportItemEvent $event) use ($output, &$progress) {
+            /** @var ImportRun $importRun */
+            $importRun = $event->getContext();
+            $processed = $importRun->getStatistics()['processed'];
+            $max = $importRun->getInfo()['count'];
+
+            if ($progress->getMaxSteps() != $max) {
+                $progress = new ProgressBar($output, $max);
+                $progress->start();
+            }
+
+            $progress->setCurrent($processed);
+        });
 
         /** @var ImportRunner $importRunner */
         $importRunner = $this->getContainer()->get('mathielen_importengine.import.runner');
-        $importRun = $importRunner->run($importConfiguration->toRun());
+        $importRunner->run($importRun);
 
-        print_r($importRun->getStatistics());
+        $progress->finish();
+        $output->writeln('');
+        $output->writeln("<info>Import done</info>");
+        $output->writeln('');
+
+        $rows = [];
+        foreach ($importRun->getStatistics() as $k=>$v) {
+            $rows[] = [$k, $v];
+        }
+
+        $table = new Table($output);
+        $table
+            ->setHeaders(array('Statistics'))
+            ->setRows($rows)
+        ;
+        $table->render();
+
+        $output->writeln('');
     }
 
     private function parseSourceId($sourceId)
     {
+        if (is_dir($sourceId) || is_file($sourceId)) {
+            return $sourceId;
+        }
+
         $sourceId = parse_url($sourceId);
         @parse_str($sourceId['query'], $sourceId['query']);
         $pathTokens = explode('.', $sourceId['path']);
