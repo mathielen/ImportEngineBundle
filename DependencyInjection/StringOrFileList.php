fa2ec8306ee900fa906a5a4144f1b572407f7b20
Markus Thielen <info@logicx.de>
2016-02-06 10:09:35 +0100

    StringOrFileList must implement Iterator and ArrayAccess

--- DependencyInjection/StringOrFileList.php
+++ DependencyInjection/StringOrFileList.php
@@ -3,19 +3,12 @@ namespace Mathielen\ImportEngineBundle\DependencyInjection;
 
 use Symfony\Component\Finder\Iterator\RecursiveDirectoryIterator;
 
-class StringOrFileList implements \ArrayAccess
+class StringOrFileList extends \ArrayObject
 {
-
-    /**
-     * @var array
-     */
-    private $listOrStringsOrFiles;
 
     public function __construct(array $listOrStringsOrFiles)
     {
-        $this->listOrStringsOrFiles = $listOrStringsOrFiles;
-
-        foreach ($this->listOrStringsOrFiles as $k=>&$v) {
+        foreach ($listOrStringsOrFiles as $k=>&$v) {
             if (is_dir($v)) {
                 $iterator = new RecursiveDirectoryIterator($v, \FilesystemIterator::KEY_AS_PATHNAME);
                 $iterator = new \RecursiveIteratorIterator($iterator);
@@ -23,40 +16,28 @@ class StringOrFileList implements \ArrayAccess
                 /** @var \Symfony\Component\Finder\SplFileInfo $file */
                 foreach ($iterator as $file) {
                     if (is_file($file)) {
-                        $this->listOrStringsOrFiles[$file->getRelativePathname()] = $file;
+                        $listOrStringsOrFiles[$file->getRelativePathname()] = $file;
                     }
                 }
 
-                unset($this->listOrStringsOrFiles[$k]);
+                unset($listOrStringsOrFiles[$k]);
             } elseif (is_file($v)) {
                 $v = new \SplFileInfo($v);
             }
         }
-    }
 
-    public function offsetExists($offset)
-    {
-        return isset($listOrStringsOrFiles[$offset]);
+        parent::__construct($listOrStringsOrFiles);
     }
 
     public function offsetGet($offset)
     {
-        $v = $this->listOrStringsOrFiles[$offset];
+        $v = parent::offsetGet($offset);
+
         if ($v instanceof \SplFileInfo) {
             return file_get_contents($v);
         }
 
         return $v;
-    }
-
-    public function offsetSet($offset, $value)
-    {
-        // TODO: Implement offsetSet() method.
-    }
-
-    public function offsetUnset($offset)
-    {
-        // TODO: Implement offsetUnset() method.
     }
 
 }
